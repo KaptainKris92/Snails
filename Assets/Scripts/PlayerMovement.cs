d9f09982b5b16eb5f8f53c61753243c3de082cc9
Kris <krisadamatzky@yahoo.co.uk>
2024-03-31 13:54:03 +0100

    Flip player sprite when moving left or right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Sprite playerSprite;
 
     private void Awake()
     {
@@ -13,6 +14,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update() // Runs on every frame of the game
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        // Flip the player sprite when moving left and right
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (horizontalInput < 0.01f)
+            transform.localScale = Vector3.one;
     }
+
+
 }
