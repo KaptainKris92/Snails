0f0876479607c8c34d0299f02766630e16265904
Kris <krisadamatzky@yahoo.co.uk>
2024-03-31 14:12:58 +0100

    Add moving animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Runtime.CompilerServices;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,23 +6,39 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Sprite playerSprite;
+    private Animator anim;
+    private Vector3 directionVector;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update() // Runs on every frame of the game
     {
+        // Horizontal input 
         float horizontalInput = Input.GetAxis("Horizontal");
 
+        Debug.Log(horizontalInput);
+
+        // Player movement based on horizontal input 
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
         // Flip the player sprite when moving left and right
-        if (horizontalInput > 0.01f)
+        if (horizontalInput > 0f)
+        {
             transform.localScale = new Vector3(-1, 1, 1);
-        else if (horizontalInput < 0.01f)
+        }
+        else if (horizontalInput < 0f)
+        {
             transform.localScale = Vector3.one;
+        }
+
+        //Set animator parameters
+        anim.SetBool("Move", horizontalInput != 0);
+
+
     }
 
 
