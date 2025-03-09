fd95c811dd07210924108fe3792f0c1fb7cf08de
KaptainKris92 <krisadamatzky@yahoo.co.uk>
2025-03-09 21:34:30 +0000

    Add firing mechanic

--- Assets/Aiming.cs
+++ Assets/Aiming.cs
@@ -6,7 +6,11 @@ public class Aiming : MonoBehaviour
 {   
     private Camera mainCam;
     private Vector3 mousePos; 
-
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire = true;
+    private float timer;
+    public float timeBetweenFiring = 0.5f;
 
 
     // Start is called before the first frame update
@@ -24,5 +28,19 @@ public class Aiming : MonoBehaviour
 
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        // If bullet collides with something, then set canFire to true. For now, this is just a timer.
+        if(!canFire){
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring){
+                canFire = true;
+            }
+
+        }
+
+        if (Input.GetMouseButton(0) && canFire){
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
     }
 }
