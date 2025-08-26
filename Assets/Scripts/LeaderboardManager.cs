d4ab454dc3c57e068c1899883290d711ac426cc7
KaptainKris92 <krisadamatzky@yahoo.co.uk>
2025-08-26 20:32:26 +0100

    Add URL for Railway Flask app

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -22,7 +22,7 @@ public class LeaderboardManager : MonoBehaviour
     public TextMeshProUGUI leaderboardText;
 
     private string playerName;
-    private string baseUrl;
+    private string baseUrl = "";
     private float playerScore;
     private bool leaderboardOnline = false;
     public static bool InputBlocked = true;
@@ -335,7 +335,7 @@ public class LeaderboardManager : MonoBehaviour
     public void SetSessionValues()
     {
         playerName = GameSessionManager.Instance.PlayerName;
-        baseUrl = GameSessionManager.Instance.NgrokURL;
+        baseUrl = "snails-production-0717.up.railway.app"; // Railway-hosted DB. 
         Debug.Log($"SetSessionValues: Name = {playerName}, baseUrl = {baseUrl}");
     }
 
